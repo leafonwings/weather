17f6ed89aaadff06b3b77d604bddaf9066431d77
leafonwinds <m18070941293@163.com>
2019-09-11 10:38:32 +0800

    change annotation

--- weather/msa-weather-mail-eureka/src/main/java/com/waylau/spring/cloud/weather/vo/MailCityReceiverConfig.java
+++ weather/msa-weather-mail-eureka/src/main/java/com/waylau/spring/cloud/weather/vo/MailCityReceiverConfig.java
@@ -1,5 +1,6 @@
 package com.waylau.spring.cloud.weather.vo;
 
 public class MailCityReceiverConfig {
-    public static String[] receivers = {"101280101!!601970471@qq.com","101280601!!qinjialong2009@qq.com"};
+    //城市id  !!为分隔符   接受邮箱地址
+    public static String[] receivers = {"101280101!!xxxxxxxxx@qq.com","101280601!!xxxxxxxx@qq.com"};
 }
